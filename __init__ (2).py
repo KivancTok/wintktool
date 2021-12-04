5fded2ef691350869c8918929e6345ccbed49652
Techsplosion <67820127+KivancTok@users.noreply.github.com>
2021-12-04 18:36:59 +0300

    Delete __init__ (2).py

--- __init__ (2).py
+++
@@ -1,4 +0,0 @@
-"""This is the official WinTkTool Malware Collection.
-Do not run any of these files in this folder, as it may cause severe damage.
-------------------------------------------------------------------------------------------
-Only test this on your VM or any of your non-primary computers setup for testing."""
